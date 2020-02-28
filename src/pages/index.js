85097cf98883ac486a95edaaf3f871bb3b38bccb
Rafael Quintanilha <rafaelquintanilha@poli.ufrj.br>
2020-02-28 18:54:32 -0300

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,46 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import useIsClient from '../useIsClient';
+import css from './index.module.css';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const ComponentWithEarlyExit = () => {
+  const { isClient, key } = useIsClient();
+  if ( !isClient ) return null;
+  return (
+    <div key={key} className={isClient ? css['red'] : css['blue']}>
+      I am in the {isClient ? "client" : "server"}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  );
+}
+
+const ComponentWithKey = () => {
+  const { isClient, key } = useIsClient();
+  return (
+    <div key={key} className={isClient ? css['red'] : css['blue']}>
+      I am in the {isClient ? "client" : "server"}
+    </div>
+  );
+}
+
+const ComponentWithoutKey = () => {
+  const { isClient } = useIsClient();
+  return (
+    <div className={isClient ? css['red'] : css['blue']}>
+      I am in the {isClient ? "client" : "server"}
+    </div>
+  );
+}
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <ComponentWithoutKey />
+      <ComponentWithKey />
+      <ComponentWithEarlyExit />
+    </Layout>
+  );
+}
 
 export default IndexPage
